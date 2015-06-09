f9c8a0ce25f32e278646db3d1d936689dcca4079
Paul Dechov <dechovsky@gmail.com>
2015-06-09 10:45:18 -0400

    Add optional GeoPath centroid param

--- src/transforms/GeoPath.js
+++ src/transforms/GeoPath.js
@@ -10,10 +10,12 @@ function GeoPath(graph) {
   Transform.addParameters(this, Geo.Parameters);
   Transform.addParameters(this, {
     field: {type: 'field', default: null},
+    centroid: {type: "value", default: "false"},
   });
 
   this._output = {
-    'path': 'layout_path'
+    "path": "layout_path",
+    "centroid": "layout_centroid"
   };
   return this;
 }
@@ -28,9 +30,13 @@ prototype.transform = function(input) {
       geojson = this.param('field').accessor || util.identity,
       proj = Geo.d3Projection.call(this),
       path = d3.geo.path().projection(proj);
+      centroid = this.param("centroid");
 
   function set(t) {
     Tuple.set(t, output.path, path(geojson(t)));
+    if (centroid) {
+      tuple.set(t, output.centroid, path.centroid(geojson(t)));
+    }
   }
 
   input.add.forEach(set);
@@ -40,6 +46,9 @@ prototype.transform = function(input) {
   }
 
   input.fields[output.path] = 1;
+  if (centroid) {
+    input.fields[output.centroid] = 1;
+  }
   return input;
 };
 
